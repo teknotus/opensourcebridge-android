2792e893f4c78db0f31c4b815455266400bc7d59
Don Park <donp@donp.org>
2014-05-16 10:29:59 -0700

    tab cleanup and eclipse java reformatting

--- src/org/opensourcebridge/android2013/Speaker.java
+++ src/org/opensourcebridge/android2013/Speaker.java
@@ -1,11 +1,11 @@
 package org.opensourcebridge.android2013;
 
 public class Speaker {
-	String name = null;
-	String biography = null;
-	String twitter = null;
-	String affiliation = null;
-	String identica = null;
-	String website = null;
-	String blog = null;
+    String name = null;
+    String biography = null;
+    String twitter = null;
+    String affiliation = null;
+    String identica = null;
+    String website = null;
+    String blog = null;
 }
